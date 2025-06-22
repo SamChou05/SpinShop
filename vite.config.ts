f07776d43df8b73c0785d32ddaa2536e20dfa3da
sam chou <sam@thechous.com>
2025-06-21 22:02:52 -0700

    fixing popup

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,10 @@ export default defineConfig({
       input: {
         background: resolve(__dirname, 'src/background/index.ts'),
         content: resolve(__dirname, 'src/content/index.ts'),
-        popup: resolve(__dirname, 'src/popup/popup.ts'),
+        popup: resolve(__dirname, 'src/popup/index.html'),
       },
       output: {
-        entryFileNames: (chunkInfo) => {
-          return chunkInfo.name === 'popup' ? 'popup.js' : '[name].js';
-        },
+        entryFileNames: '[name].js',
         chunkFileNames: '[name].js',
         assetFileNames: '[name].[ext]'
       }
